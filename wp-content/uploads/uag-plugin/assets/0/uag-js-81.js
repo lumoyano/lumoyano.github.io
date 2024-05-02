0636c9e547e17d6358061bf9b1a3013c56b01dc1
Lucas Almeida <121978205+lumoyano@users.noreply.github.com>
2024-05-02 17:47:08 -0600

    Add files via upload

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-81.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBTimelineClasses( {"block_id":"ef54be12","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-ef54be12' );
+});
+window.addEventListener("resize", function(){
+	UAGBTimelineClasses( {"block_id":"ef54be12","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-ef54be12' );
+});
+ });
---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-82.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBTimelineClasses( {"block_id":"a069d90c","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-a069d90c' );
+});
+window.addEventListener("resize", function(){
+	UAGBTimelineClasses( {"block_id":"a069d90c","timelinAlignment":"center","timelinAlignmentTablet":"left","timelinAlignmentMobile":"left"}, '.uagb-block-a069d90c' );
+});
+ });
